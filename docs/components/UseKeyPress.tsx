b8cbdaeca08646695cd4630580bdb2278a481385
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-08-25 09:28:14 +0630

    doc: add onKeyDown doc

--- docs/components/UseKeyPress.tsx
+++ docs/components/UseKeyPress.tsx
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
-import { useKeyPress } from "@reactusehooks/core";
+import React, {useState} from "react";
+import {useKeyPress} from "@reactusehooks/core";
 import PlayGround from "@/components/cards/PlayGround";
 
 const UseKeyPress = () => {
-  const [isClicked, setIsClicked] = useState(false);
-  useKeyPress("ArrowDown", () => setIsClicked(true));
+    const [isClicked, setIsClicked] = useState(false);
+    useKeyPress("ArrowDown", () => setIsClicked(true));
 
-  return (
-    <PlayGround>
-      <div>{isClicked ? "Arrow Down Key was clicked" : "Not Clicked Yet"}</div>
-    </PlayGround>
-  );
+    return (
+        <PlayGround>
+            <div>
+                Please click the <b>Arrow Down</b> key
+            </div>
+            <div>{isClicked ? "Arrow Down Key was clicked" : "Not Clicked Yet"}</div>
+        </PlayGround>
+    );
 };
 
 export default UseKeyPress;
